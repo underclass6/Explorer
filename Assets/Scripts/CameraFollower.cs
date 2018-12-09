6628363d9d6bb562608fb61c9739eb58a9fc490d
underclass <584169921@qq.com>
2018-12-09 22:14:25 +0800

    The first time to commit, just a simple demo.
    add

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+    // 引用对象
+    public Transform target;
+
+    // 字段
+    private Vector3 target2D; // 三维空间转换为二维
+    private float startDistance = 1e-4f; // 开始跟踪的距离
+    private Vector3 currentVelocity = new Vector3(0, 0, 0);
+    private float followTime = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+        target2D = new Vector3(0, 0, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Follow(); // 相机进行跟踪
+	}
+
+    private void Follow()
+    {
+        // target2D获得目标对象的横纵坐标
+        target2D.x = target.position.x;
+        target2D.y = target.position.y;
+
+        // 相机和跟踪对象间的距离
+        float disTarAndCamera = Mathf.Pow(target2D.x - transform.position.x, 2) + Mathf.Pow(target2D.y - transform.position.y, 2);
+
+        bool startFollow = disTarAndCamera > startDistance;
+
+        if (startFollow)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target2D, ref currentVelocity, followTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Traveler.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traveler : MonoBehaviour {
+    // 引用
+    public Sprite[] sprites;
+
+    // 字段
+    private float speed;
+    private int directIndex = 0;
+    private Animator animator;
+    private Vector3 currentPos;
+    private SpriteRenderer spr;
+
+	// Use this for initialization
+	void Start () {
+        currentPos = new Vector3(0, 0, 0);
+        speed = 1.5f;
+        animator = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Moving();
+	}
+
+    void LateUpdate()
+    {
+        if (!Input.anyKey)
+            spr.sprite = sprites[directIndex];
+    }
+
+    private void Moving()
+    {
+        currentPos = transform.position;
+        if (Input.anyKey)
+        {
+            setAnim();
+            if (Input.GetKey(KeyCode.W))
+            {
+                currentPos.y += speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                currentPos.y -= speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                currentPos.x -= speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                currentPos.x += speed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            animator.SetInteger("direction", 0);
+        }
+        transform.position = currentPos;
+    }
+
+    private void setAnim()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            directIndex = 1;
+            animator.SetInteger("direction", 2);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            directIndex = 0;
+            animator.SetInteger("direction", 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            directIndex = 2;
+            animator.SetInteger("direction", 3);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            directIndex = 3;
+            animator.SetInteger("direction", 4);
+        }
+    }
+}
